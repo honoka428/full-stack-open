c680efb95dd94dd23a2a19453d81d67545433f15
Honoka Watanabe <honokawatanabe@Honokas-MacBook-Air.local>
2020-05-01 15:11:39 +0200

    Exercise 1.7 completed. Add statistics

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 
 function App() {
   const [clicks, setClicks] = useState({
-    left: 0, middle: 0, right: 0
+    left: 0, middle: 0, right: 0, all: 0
   })
 
-  const handleLeftClick = () => { setClicks({...clicks, left: clicks.left + 1 }) }
-  const handleMiddleClick = () => { setClicks({...clicks, middle: clicks.middle + 1 }) }
-  const handleRightClick = () => { setClicks({...clicks, right: clicks.right + 1 }) }
+  const handleLeftClick = () => { setClicks({...clicks, left: clicks.left + 1, all: clicks.all + 1 }) }
+  const handleMiddleClick = () => { setClicks({...clicks, middle: clicks.middle + 1, all: clicks.all + 1 }) }
+  const handleRightClick = () => { setClicks({...clicks, right: clicks.right + 1, all: clicks.all + 1 }) }
 
   return (
     <div>
@@ -17,20 +17,20 @@ function App() {
         <h1> give feedback </h1>
         <div>
           <button onClick={handleLeftClick}>good</button> 
-          
           <button onClick={handleMiddleClick}>neutral</button> 
-          
           <button onClick={handleRightClick}>bad</button> 
 
         </div>
       </div>
 
       <div>
-      <h1> statistics </h1>
-      <p> good {clicks.left}</p>
-      <p> neutral {clicks.middle}</p>
-      <p> bad {clicks.right}</p>
-
+        <h1> statistics </h1>
+        <p> good {clicks.left}</p>
+        <p> neutral {clicks.middle}</p>
+        <p> bad {clicks.right}</p>
+        <p> all {clicks.all}</p>
+        <p> average {((1 * clicks.left) + 0 + (-1 * clicks.middle)) / clicks.all} </p>
+        <p> positive {clicks.right / clicks.all}% </p>
       </div> 
     </div>      
   );
