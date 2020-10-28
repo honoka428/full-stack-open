0ed143abf3c149b69aebbd5cf8217f463a3796c9
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-10-28 12:56:12 -0400

    Exercise 2.13 complete

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,46 +1,75 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = (country) => <div>{country.country.name}</div>
+const Country = (props) => 
+    <div style={{paddingTop: 20, display: 'flex'}}>
+      <div>{props.country.name}   </div>
+      <button type="button" style={{marginLeft: 10}} onClick={props.handleClick} value={props.country.name}>show</button>
+    </div>
   
 const App = () => {
 
   const [countries, setCountries] = useState([])
-  const [showAll, setShowAll] = useState(true)
   const [newFilter, setNewFilter] = useState('')
-
+  const [filtered, setFiltered] = useState(false)
+  
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
 
-  console.log(countries[0])
+  var filteredCountries = countries.filter(country => country.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1)
+
+  const handleShowBtnClick = (event) => {
+    setNewFilter(event.target.value)
+    setFiltered(true)
+  }
   
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
-    setShowAll(false)
+    setFiltered(true)
   }
 
-  const countriesToShow = showAll 
-    ? countries
-    : countries.filter(country => country.name.toLowerCase().indexOf(newFilter.toLowerCase()) !== -1)
-  
-  return (
-    <div className="App">
+  const DefaultSearchResult = countries.map( country => <Country country={country} key={country.numericCode} handleClick={handleShowBtnClick}/> )
+    
+  var FilteredSearchResult = filteredCountries.map( country => <Country country={country} key={country.numericCode} handleClick={handleShowBtnClick}/> )
+
+  if (filteredCountries.length >= 10) {
+    FilteredSearchResult = <div>Too many matches, specify another filter.</div>
+  }
+
+  if (filteredCountries.length === 0) {
+    FilteredSearchResult = <div> No matches, try a different search. </div>
+  }
+
+  if (filteredCountries.length === 1) {
+    FilteredSearchResult = 
       <div>
+        <h1>{filteredCountries[0].name}</h1>
+        <div> Capital: {filteredCountries[0].capital}</div>
+        <div> Population: {filteredCountries[0].population}</div>
+        <div>
+          <h2>Languages:</h2>
+          <ul>{ filteredCountries[0].languages.map(language => <li key={language.iso639_1}>{language.name}</li>)}</ul>
+        </div>
+        <img src={filteredCountries[0].flag} style={{height: 200}}/>
+      </div>
+  }
+
+  const CountriesToShow = () => filtered
+    ? FilteredSearchResult
+    : DefaultSearchResult
+
+  return (
+    <div className="App" style={{margin: 20}}>
+      <div style={{marginBottom: 10}}>
         find countries <input value={newFilter}  onChange={handleFilterChange}/>
       </div>
       <div>
-        {
-          countriesToShow.map( country =>
-            <Country country={country} key={country.numericCode}/>
-          )
-        }
+        <CountriesToShow/>
       </div>
     </div>
   );
