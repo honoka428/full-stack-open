bdc8ad9a64f7d87f4b33cb2d24de6c5c9b432958
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-11-02 15:55:06 -0500

    Update api to related url for prod

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'https://enigmatic-stream-93396.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
