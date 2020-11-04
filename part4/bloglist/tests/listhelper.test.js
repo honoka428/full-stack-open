42a277a7e277ae14d29b5d7d1ee9a416d41e193d
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-11-04 18:00:09 -0500

    Exercise 4.7 complete

--- part4/bloglist/tests/listhelper.test.js
+++ part4/bloglist/tests/listhelper.test.js
@@ -8,7 +8,7 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-describe('Likes', () => {
+describe('Blog info', () => {
   const blogs = [
     { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
     { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
@@ -18,7 +18,7 @@ describe('Likes', () => {
     { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
   ]
 
-  test('total likes', () => {
+  test('sum of all likes', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
   })
@@ -31,7 +31,7 @@ describe('Likes', () => {
   })  
 })  
 
-describe('Blog Count', () => {
+describe('Author info', () => {
   const blogs = [
     { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
     { _id: "5a422aa71b54a676234d17f8", title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra", url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0 },
@@ -41,13 +41,23 @@ describe('Blog Count', () => {
     { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
   ]
 
-  test('author with most blogs', () => {
+  test('highest blog count', () => {
     const result = listHelper.mostBlogs(blogs)
     expect(result).toEqual(
       {
         author: "Robert C. Martin",
         blogs: 3
       }      
+    )
+  })
+
+  test('most liked author', () => {
+    const result = listHelper.mostLiked(blogs)
+    expect(result).toEqual(
+      {
+        author: "Edsger W. Dijkstra",
+        likes: 17
+      }    
     )
   })
 })
