e0e3e65df6584c54d628bd56a1646299e840142a
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-11-04 17:10:49 -0500

    Exercis 4.4 complete

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -1,7 +1,20 @@
+const blogsRouter = require("../controllers/blogs")
+
 const dummy = (blogs) => {
     return 1
   }
   
+const totalLikes = (blogs) => {
+    var sum = 0
+
+    blogs.forEach(blog => {
+        sum += blog.likes
+    })
+
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
