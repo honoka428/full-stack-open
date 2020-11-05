638352a76416dabd5834dd0def9aa1d9a9207543
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-11-05 15:08:02 -0500

    Exercise 4.9 complete

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(note => note.save())
+    await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  })
-  
-  afterAll(() => {
+})
+
+test('id is defined', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+})
+
+afterAll(() => {
     mongoose.connection.close()
-  })
+})
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,18 @@
+// const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'Test Title',
+        author: 'John Doe',
+        url: 'https:w.com',
+        likes: 4
+    },
+    {
+        title: 'Second Title',
+        author: 'Smith Jones',
+        url: 'https:rwefd.com',
+        likes: 1
+    },
+]
+
+module.exports = {initialBlogs}
