f5a16fdef46c6b5cbb4d26716fd4119532175bad
Honoka Watanabe <watanabehonoka428@gmail.com>
2020-11-10 12:00:34 -0500

    Exercise 5.7 complete

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+
+  const [ detailsVisible, setDetailsVisible] = useState(false)
+
+  const toggleDetailsView = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  const blogNameOnly = () =>
+    <div>
+      {blog.title} <button onClick={toggleDetailsView}> view </button>
+    </div>
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const allBlogDetails = () => 
+    <div>
+      {blog.title} <button onClick={toggleDetailsView}> hide </button> <br/>
+      url: {blog.url} <br/>
+      author: {blog.author} <br/>
+      likes: {blog.likes} <button>like</button>
+    </div>
+
+  return (
+    <div style={blogStyle}>
+      { detailsVisible === true
+          ? allBlogDetails()
+          : blogNameOnly()
+      }
+    </div>
+
+  )
+}
 
 export { Blog }
